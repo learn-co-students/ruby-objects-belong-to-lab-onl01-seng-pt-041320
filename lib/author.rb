b0b6a55c83118960fd7963e9c64d7b42ba753997
eulis01 <eulis01@gmail.com>
2020-05-04 20:55:12 -0400

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,4 @@
+# Created a class for the Author take in a :name Argument
+class Author
+    attr_accessor :name # Created a setter and getter 
+end
---
+++ lib/song.rb
@@ -0,0 +1,4 @@
+# Defined a class Song with 2 Arguments a :title and :artist
+class Song
+    attr_accessor :title, :artist # Defining the setter and getter method
+end
