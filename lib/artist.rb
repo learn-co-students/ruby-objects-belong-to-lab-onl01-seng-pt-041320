363938f41405759a6892701460e06aa1032e2f15
eulis01 <eulis01@gmail.com>
2020-05-04 20:56:54 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,4 @@
 # Created a class for the Artist takes a :name Argument
 class Artist
-attr_accessor :name  # Created a  setter and getter.
+    attr_accessor :name  # Created a  setter and getter.
 end
